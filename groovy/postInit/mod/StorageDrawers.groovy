238ea07e606e563081549139e6cee4f09d74a509
trainvoi <voitrainelephant@gmail.com>
2024-11-25 22:06:38 +0700

    re-enable controller

--- groovy/postInit/mod/StorageDrawers.groovy
+++ groovy/postInit/mod/StorageDrawers.groovy
@@ -1,7 +1,7 @@
 def name_removals = [
     'storagedrawers:compacting_drawer',
-    'storagedrawers:controller',
-    'storagedrawers:controller_slave',
+    #'storagedrawers:controller',
+    #'storagedrawers:controller_slave',
     'storagedrawers:upgrade_storage_gold',
     'storagedrawers:upgrade_storage_diamond',
     'storagedrawers:upgrade_storage_emerald',
